35812335304e77a8746ea011586a11002c8bbc87
mirko.richter@mgm-sp.com <mirko.richter@mgm-sp.com>
2026-03-05 13:56:43 +0100

    initial commit

---
+++ modules/filters/rrfunc.js
@@ -0,0 +1,29 @@
+/*\
+title: $:/plugins/rimir/typed/modules/filters/rrfunc.js
+type: application/javascript
+module-type: filteroperator
+
+Filter operator returning those input titles that are returned from a function
+
+\*/
+
+"use strict";
+
+/*
+Export our filter function
+*/
+exports.rrfunc = function(source,operator,options) {
+	var functionName = operator.operands[0],
+		params = [],
+		results;
+	$tw.utils.each(operator.operands.slice(1),function(param) {
+		params.push({value: param});
+	});
+	// console.log(`Calling ${functionName} with params ${JSON.stringify(params)}`);
+	var variableInfo = options.widget && options.widget.getVariableInfo && options.widget.getVariableInfo(functionName,{params: params, source: source});
+	if(variableInfo && variableInfo.srcVariable && variableInfo.srcVariable.isFunctionDefinition) {
+		results = variableInfo.resultList ? variableInfo.resultList : [variableInfo.text];
+	}
+	// console.log(`function ${functionName} with params ${JSON.stringify(params)} results: ${JSON.stringify(results)}`);
+	return results || [];
+};
